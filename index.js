4bdab491f293887dcccfae8b8ec76b4f965403c0
HeerKukreja <2017.heer.kukreja@ves.ac.in>
2022-04-02 14:18:01 +0530

    Cors error solved

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 require("dotenv").config({ path: "./.env" });
 const express = require("express");
 var mongoose = require("mongoose");
+var cors = require("cors");
 var app = express();
+
+var allowedOrigins = [
+  "http://localhost:8000",
+  "https://text-crud.herokuapp.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 //Route
 app.get("/", function (req, res) {
   res.send("hello world");
